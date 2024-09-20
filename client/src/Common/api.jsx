81c71c22a23e279187543153bf6e49ba254fa00d
DIVYANSH SINGH RATHORE <divyanshsrathorez@gmail.com>
2024-09-20 19:26:55 +0530

    adding url

--- client/src/Common/api.jsx
+++ client/src/Common/api.jsx
@@ -2,58 +2,58 @@ import { handleError } from "./configurations";
 import axios from "axios";
 
 // export const URL = "https://exiphones.shop/api";
-export const URL = "http://localhost:4000/api";
+export const URL = "http://13.234.82.121/api";
 
 const apiInstance = axios.create({
-  baseURL: URL,
+	baseURL: URL,
 });
 
 // Response interceptor
 apiInstance.interceptors.response.use((response) => {
-  // You can modify the response data here
-  return response.data;
+	// You can modify the response data here
+	return response.data;
 });
 
 export const commonReduxRequest = async (
-  method,
-  route,
-  body,
-  config,
-  rejectWithValue
+	method,
+	route,
+	body,
+	config,
+	rejectWithValue
 ) => {
-  let requestConfig = {
-    method,
-    url: route,
-    data: body,
-    headers: config,
-    withCredentials: true,
-  };
+	let requestConfig = {
+		method,
+		url: route,
+		data: body,
+		headers: config,
+		withCredentials: true,
+	};
 
-  try {
-    const response = await apiInstance(requestConfig);
+	try {
+		const response = await apiInstance(requestConfig);
 
-    return response;
-  } catch (error) {
-    console.log(error);
-    return handleError(error, rejectWithValue);
-  }
+		return response;
+	} catch (error) {
+		console.log(error);
+		return handleError(error, rejectWithValue);
+	}
 };
 
 export const commonRequest = async (method, route, body, config) => {
-  let requestConfig = {
-    method,
-    url: route,
-    data: body,
-    headers: config,
-    withCredentials: true,
-  };
+	let requestConfig = {
+		method,
+		url: route,
+		data: body,
+		headers: config,
+		withCredentials: true,
+	};
 
-  try {
-    const response = await apiInstance(requestConfig);
+	try {
+		const response = await apiInstance(requestConfig);
 
-    return response;
-  } catch (error) {
-    console.log(error);
-    return error;
-  }
+		return response;
+	} catch (error) {
+		console.log(error);
+		return error;
+	}
 };
